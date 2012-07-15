b49e253eb193ae4472596b3f60c4c4acc65d5fa1
Adrián E. Piñeyro <adrian_ep@msn.com>
2012-07-15 20:01:07 -0300

    Added GraphView & Controller, Axis support Class Files, Embed in Navigation Controller, Segue calculator to graph controller

---
+++ Calculator/GraphView.h
@@ -0,0 +1,13 @@
+//
+//  GraphView.h
+//  Calculator
+//
+//  Created by Adrián Piñeyro on 15/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphView : UIView
+
+@end
---
+++ Calculator/GraphViewController.h
@@ -0,0 +1,16 @@
+//
+//  GraphViewController.h
+//  Calculator
+//
+//  Created by Adrián Piñeyro on 15/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GraphView.h"
+
+@interface GraphViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet GraphView *graphView;
+
+@end
